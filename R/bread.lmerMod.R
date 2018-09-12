a9b6a43809f60e10c4a324c31ea4a91268a59807
Ting Wang <twb8d@mail.missouri.edu>
2018-09-12 20:20:03 +0000

    version 0.1-5

--- R/bread.lmerMod.R
+++ R/bread.lmerMod.R
@@ -1,9 +1,25 @@
-bread.lmerMod <- function (object, full = TRUE, information = "expected", ...){  
+bread.lmerMod <- function (x, ...){  
   ## get all elements by getME and exclude multiple random effect models.
-  if (!is(object, "lmerMod")) stop("bread.lmerMod() only works for lmer() models.")
+  if (!is(x, "lmerMod")) stop("bread.lmerMod() only works for lmer() models.")
   
-  parts <- getME(object, "ALL")
+  parts <- getME(x, "ALL")
   if (length(parts$l_i) > 1) stop("Multiple cluster variables detected. Robust SEs are unavailable.")
   
+  dotdotdot <- list(...)
+  if("full" %in% names(dotdotdot)){
+    full <- dotdotdot$full
+  } else {
+    full <- FALSE
+  }
+  if ("information" %in% names(dotdotdot)) {
+    information <- dotdotdot$information
+  } else {
+    information <- "expected"
+  }
+  
+if(!(full %in% c("TRUE", "FALSE"))) stop("invalid 'full' argument supplied")
+if(!(information %in% c("expected", "observed"))) stop("invalid 'information' argument supplied")
+  
+  object <- x
   return(vcov.lmerMod(object, full = full, information = information) * parts$l_i)
 }
